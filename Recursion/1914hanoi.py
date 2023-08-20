f9eb216ad003e91a05894a432c78580fe48e12be
Jackie <101847615+jac148@users.noreply.github.com>
2023-08-20 19:13:25 +0900

    출력 초과 해결
    n>20 까먹었다

--- Recursion/1914hanoi.py
+++ Recursion/1914hanoi.py
@@ -1,12 +1,14 @@
-def hanoi(n, start, end, middle):
+def hanoi(n, start, end):
     if n == 1:
         print(start, end) #move disk 1 from start to end
         return
     
-    hanoi(n-1, start, middle, end ) # moving (n-1)disks from start to middle using end as an auxiliary
+    hanoi(n-1, start, 6-start-end ) # moving (n-1)disks from start to middle using end as an auxiliary
     print(start, end)  #'n'th disk is moved from start to end
-    hanoi(n-1, middle, end, start) #move the (n-1)disks from middle to end, using start as an auxiliaty
+    hanoi(n-1, 6-start-end, end) #move the (n-1)disks from middle to end, using start as an auxiliaty
 
 n = int(input())
 print(2**n-1)  
-hanoi(n, 1, 3, 2 ) # peg 1 and 3 as the start and end peg
+if n <= 20:
+    hanoi(n, 1, 3 ) # peg 1 and 3 as the start and end peg
+
